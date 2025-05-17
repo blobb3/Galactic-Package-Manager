c2097e933599a49b40f8c306bfb66d3e0aab6169
blobb3 <janine.heinemeyer@gmail.com>
2025-05-17 23:39:33 +0200

    EInrichten abgeschlossen

---
+++ backend/galactic-pm/src/main/java/ch/zhaw/devops/gpm/galactic_pm/GalacticPmApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.devops.gpm.galactic_pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalacticPmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalacticPmApplication.class, args);
+	}
+
+}
